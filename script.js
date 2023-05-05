bd9954148e0c9d6f40fdfcb320ba3a518d16343a
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-05 03:42:07 -0400

    Change values of variables to strings and have the function return one of the three variables

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function getComputerChoice() {
-    let r = 1;
-    let p = 2;
-    let s = 3;
+    let r = "Rock";
+    let p = "Paper";
+    let s = "Scissors";
     let randomInterger = Math.floor(Math.random() * 3) + 1;
 
     if (randomInterger === 1) {
-        return "Rock";
+        return r;
     } else if (randomInterger === 2) {
-        return "Paper";
+        return p;
     } else {
-        return "Scissors";
+        return s;
     }
 }
