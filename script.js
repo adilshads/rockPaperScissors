51f0f7219e2df196bd8208cc4dc6bc649beb0fe6
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-05 20:31:23 -0400

    Fix syntax errors

--- script.js
+++ script.js
@@ -15,26 +15,26 @@ function getComputerChoice() {
 
 
 function rockPaperScissors(playerSelection, computerSelection) {
-    let computerSelection = getComputerChoice;
+    let computerSelection = getComputerChoice();
     let playerSelection = prompt("Rock, Paper, Scissors?").toUpperCase();
 
-    if (computerSelection === "ROCK" && playerSelection === "ROCK"); {
+    if (computerSelection === "ROCK" && playerSelection === "ROCK") {
         return "Tie Game!";
-    } else if (computerSelection === "ROCK" && playerSelection === "PAPER"); {
+    } else if (computerSelection === "ROCK" && playerSelection === "PAPER") {
         return "You Win!";
-    } else if (computerSelection === "ROCK" && playerSelection === "SCISSORS"); {
+    } else if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
         return "You Lose!";
-    } else if (computerSelection === "PAPER" && playerSelection === "PAPER"); {
+    } else if (computerSelection === "PAPER" && playerSelection === "PAPER") {
         return "Tie Game!";
-    } else if (computerSelection === "PAPER" && playerSelection === "SCISSORS"); {
+    } else if (computerSelection === "PAPER" && playerSelection === "SCISSORS") {
         return "You Win!";
-    } else if (computerSelection === "PAPER" && playerSelection === "ROCK"); {
+    } else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
         return "You Lose!";
-    } else if (computerSelection === "SCISSORS" && playerSelection === "SCISSORS"); {
+    } else if (computerSelection === "SCISSORS" && playerSelection === "SCISSORS") {
         return "Tie Game!";
-    } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK"); {
+    } else if (computerSelection === "SCISSORS" && playerSelection === "ROCK") {
         return "You Win!";
-    } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER"); {
+    } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
         return "You Lose!";
     }
 }
